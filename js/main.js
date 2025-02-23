d3745a100e4d70b4813d2967ae8205918535874d
Amir3629 <amirhossein.firoozi@hotmail.com>
2025-02-23 22:43:28 +0100

    Add complete website files with all content

---
+++ js/main.js
@@ -0,0 +1,11 @@
+﻿// Smooth scrolling for navigation links
+document.querySelectorAll("a[href^=\"#\"]").forEach(anchor => {
+    anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
+// Rest of the JavaScript content
