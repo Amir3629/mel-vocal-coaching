f9b2754d2e54c59b4f676ce792bba0430beae45e
Amir3629 <amirhossein.firoozi@hotmail.com>
2025-02-28 21:19:54 +0100

    Updated website content and design

---
+++ index.js
@@ -0,0 +1,5 @@
+const addon = require('./build/Release/addon');
+
+console.log(addon.hello()); // This should print "Hello from C++!"
+
+// Additional code to start your web server or application logic can be added here.
---
+++ node-v22.14.0/node-v22.14.0/test/js-native-api/test_async/test.js
@@ -0,0 +1,3 @@
+(function childMain() {
+  return;
+})(); 
